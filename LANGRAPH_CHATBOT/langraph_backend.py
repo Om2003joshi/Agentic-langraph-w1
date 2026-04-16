0588a0970fb72c2a994d44af8d701791b1800f0a
Bhavesh0809 <om2003joshi@gmail.com>
2026-04-16 14:02:03 +0530

    solving override error

--- LANGRAPH_CHATBOT/langraph_backend.py
+++ LANGRAPH_CHATBOT/langraph_backend.py
@@ -1,13 +1,14 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage,HumanMessage
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
-
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
 # Load environment variables from .env
-load_dotenv()
-
+load_dotenv(override=True)
+print("LOADED KEY:", os.environ.get("GOOGLE_API_KEY"))
 # Initialize the LLM
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",                   # correct parameter
@@ -32,3 +33,12 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+for message_chunk , metadata in chatbot.stream(
+    {"messages": [HumanMessage(content="what is recipe to make pasta?")]},
+    config={'configurable': {'thread_id': 'thread-1'}},
+    stream_mode="messages"
+    ):
+    
+    if message_chunk.content:
+        print(message_chunk.content,end="",flush=True)
